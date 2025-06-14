fff6a2516239fe3562c76c081b35dcb8104dfbba
santidemo <abansantiago2.0@gmail.com>
2025-06-14 16:00:28 -0300

    Cambios solo en la parte de pagina principal

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,21 @@
+from flask import Flask, send_from_directory, request, jsonify
+import os
 
-from flask import Flask, render_template
 
-app = Flask(__name__)
+# Inicializamos la app
+app = Flask(__name__, static_folder='static')
 
-@app.route('/')
-def pagina_principal():
-    return render_template('pagina-prin.html')
+# 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    full_path = os.path.join(app.static_folder, 'browser', path)
+
+    if path != "" and os.path.exists(full_path):
+        return send_from_directory(os.path.join(app.static_folder, 'browser'), path)
+    else:
+        return send_from_directory(os.path.join(app.static_folder, 'browser'), 'index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ backend/conexion.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+
+def get_connection():
+    try:
+        connection = psycopg2.connect(
+            host='localhost',
+            database='GoPark',  # Nombre de tu base de datos
+            user='postgres',
+            password='Aban12062007'  # Cambia esto
+        )
+        return connection
+    except Exception as e:
+        print("Error al conectar a la base de datos:", e)
+        return None
