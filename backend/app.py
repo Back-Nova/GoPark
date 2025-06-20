7bc86b789d4e7b79ef9a056ba52d75812f75dd1a
santidemo <abansantiago2.0@gmail.com>
2025-06-20 13:49:29 -0300

    Cambio

--- backend/app.py
+++ backend/app.py
@@ -396,5 +396,4 @@ def obtener_reservas():
 # ------------------ ARRANCAR SERVIDOR -------------------
 if __name__ == '__main__':
     print("Servidor Flask arrancando...")
-    app.run(debug=True, port=5000)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
